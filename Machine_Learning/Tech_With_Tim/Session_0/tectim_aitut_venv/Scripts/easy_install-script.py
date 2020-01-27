e227753627c626599c01fd6afd1dc4cb4d76910e
CyborgVillager <almawijonathan@gmail.com>
2020-01-26 20:10:12 -0600

    v0 Intro to Ai Dev Everything has it's Beginning
    Intro to Ai Dev Everything has it's Beginning: Will be completing a video from Tech With Tim in learning the
                                                        different areas of Machine learning/Ai
                                                        His Desc:
                                                        Ready to explore machine learning and artificial intelligence in python?
                                                        This python machine learning and AI mega course contains 4 different
                                                        series designed to teach you the ins and outs of ML and AI.
                                                        It talks about fundamental ML algorithms, neural networks, creating
                                                        AI chat bots and finally developing an AI that can play the game of Flappy Bird.
                                               Video Link: https://www.youtube.com/watch?v=WFr2WgN9_xE

---
+++ Machine_Learning/Tech_With_Tim/Session_0/tectim_aitut_venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!D:\PyProject\Machine_Learning\Tech_With_Tim\Session_0\tectim_aitut_venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install')()
+    )
---
+++ Machine_Learning/Tech_With_Tim/Session_0/tectim_aitut_venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!D:\PyProject\Machine_Learning\Tech_With_Tim\Session_0\tectim_aitut_venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip')()
+    )
