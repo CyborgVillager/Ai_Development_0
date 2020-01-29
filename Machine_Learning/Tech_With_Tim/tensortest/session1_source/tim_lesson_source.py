f7f7b75a2c3bc0258321527c0aba7be5be9e0333
CyborgVillager <almawijonathan@gmail.com>
2020-01-29 09:47:50 -0600

    v4 Beta version .5 Prediction Implemented
    Beta version .5 Prediction Implemented: The program will now train it self 4 90% & do test 4 10%
                                    Line 22 @ test.py takes all of the attributes from Features & Labels ->
                                    the program will split them up into 4 arrays  x_train -> section of numpy.array
                                    (data.drop([predictiction],1)) , y_train -> numpy.array(data[predictiction])
                                     x_test & y_test will test the accuracy of the program/model.
    
                                     Imported import sklearn to session1_source/tim_lesson_source.py
                                     Program has basic prediction, of the excel file student-mat.csv, will upgrade further

--- Machine_Learning/Tech_With_Tim/tensortest/session1_source/tim_lesson_source.py
+++ Machine_Learning/Tech_With_Tim/tensortest/session1_source/tim_lesson_source.py
@@ -1,6 +1,7 @@
 import tensorflow
 import keras
 import numpy
+import sklearn
 import pandas
 from sklearn import linear_model
 from sklearn.utils import shuffle
--- Machine_Learning/Tech_With_Tim/tensortest/test.py
+++ Machine_Learning/Tech_With_Tim/tensortest/test.py
@@ -1,8 +1,24 @@
 from tim_lesson_source import *
 
 data = pandas.read_csv('../Excel/student-mat.csv', sep=';')
-data = data[['G1','G2','G3','studytime','failures','absences']]
-print(data)
-# print 1st 5 elements -> data.head()
+# Known as an atribute
+data = data[['G1','G2','G3','studytime','sex','age','health','Pstatus','failures','absences']]
+
+# Result
 print('---------    ---------   ---------   ---------')
-print(data.head())
+#print(data.head())
+
+# G3 = Final Grade
+# program will 'predict' the atribute
+predictiction = 'G3'
+
+# The Features
+X = numpy.array(data.drop([predictiction],1))
+# Labels
+Y = numpy.array(data[predictiction])
+
+# The program will now train it self 4 90% & do test 4 10%
+# Line 22 takes all of the attributes from Features & Labels -> the program will split them up into 4 arrays
+# x_train -> section of numpy.array(data.drop([predictiction],1)) , y_train -> numpy.array(data[predictiction])
+# x_test & y_test will test the accuracy of the program/model.
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
