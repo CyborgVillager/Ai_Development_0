12119e59f931feeedd6db13f6ba083c67320fa8b
CyborgVillager <almawijonathan@gmail.com>
2020-01-28 21:34:59 -0600

    v3 Pip install
    Pip install: installed sklearn, panda, & numpy, made a folder to hold .csv files. test.py now prints out the
                        csv files. Will work on making the program predict on 1/29/20

---
+++ Machine_Learning/Tech_With_Tim/tensortest/session1_source/tim_lesson_source.py
@@ -0,0 +1,6 @@
+import tensorflow
+import keras
+import numpy
+import pandas
+from sklearn import linear_model
+from sklearn.utils import shuffle
--- Machine_Learning/Tech_With_Tim/tensortest/test.py
+++ Machine_Learning/Tech_With_Tim/tensortest/test.py
@@ -1,2 +1,8 @@
-import tensorflow
-import keras
+from tim_lesson_source import *
+
+data = pandas.read_csv('../Excel/student-mat.csv', sep=';')
+data = data[['G1','G2','G3','studytime','failures','absences']]
+print(data)
+# print 1st 5 elements -> data.head()
+print('---------    ---------   ---------   ---------')
+print(data.head())
