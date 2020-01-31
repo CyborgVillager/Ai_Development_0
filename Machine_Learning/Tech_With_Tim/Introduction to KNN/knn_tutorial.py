c8db6ea10ceef7a4e4974da61fc67909fc03f2ac
CyborgVillager <almawijonathan@gmail.com>
2020-01-31 11:37:38 -0600

    v13 Basic Results
    Basic Results: Results from car.data can be organized/displayed regarding their category @ file knn_tutorial.py
                        more work is needed to refine, will update by the end of 1/31/20

--- Machine_Learning/Tech_With_Tim/Introduction to KNN/knn_tutorial.py
+++ Machine_Learning/Tech_With_Tim/Introduction to KNN/knn_tutorial.py
@@ -1,4 +1,32 @@
 from knn_source import *
 
+# statements for data[]
+buying = 'buying'
+maint = 'maint'
+persons = 'persons'
+lug_boot = 'lug_boot'
+safety = 'safety'
+cls = 'class'
+
 data = pd.read_csv('car_data_set/car.data')
 print(data.head())
+
+
+
+# new objects these will return a numpy array
+# turn
+labencode = preprocessing.LabelEncoder()
+# making an array for each column
+buying = labencode.fit_transform(list(data[buying]))
+maint = labencode.fit_transform(list(data[maint]))
+persons = labencode.fit_transform(list(data[persons]))
+lug_boot = labencode.fit_transform(list(data[lug_boot]))
+safety = labencode.fit_transform(list(data[safety]))
+cls = labencode.fit_transform(list(data[cls]))
+
+# Prediction Varia
+prediction = 'class'
+
+# Result
+print(buying)
+
