f56111781df83ef11210e0c7e13e56bf6addfb43
CyborgVillager <almawijonathan@gmail.com>
2020-02-01 11:15:06 -0600

    v19 Learning On How Support Vector Machine Works
    Learning On How Support Vector Machine Works: ' support vector machine works by dividing data into multiple classes
                                                        using something called a hyper-plane. A hyper plane is a fancy word
                                                        for something that is straight that can divide data points. In 2D space
                                                         a hyper-plane is simply a line, in 3D space a hyper-plane is a plane.
                                                          In any space higher than 3D it is simply called a hyper-plane.'
                                                          ~ Tim
                                                          Link: https://techwithtim.net/tutorials/machine-learning-python/svm-2/
                                                              The larger the distance / easier to seperate the points and classify
                                                          it at a better prediction on what it is.
    
                                                         Kernels:
                                                         'Kernels provide a way for us to create a hyper-plane for data like seen above.
                                                          We use a kernel to bring our data up to a higher dimension (in this case from 2D->3D).
                                                          We hope that by doing this we will have our points plotted in a way that we
                                                          can divide them using a hyper-plane.'
    
                                                          ~ Tim
                                                          Function for kernel f(x1,x2) -> x3 or f(x1,x2) = x3

--- Machine_Learning/Tech_With_Tim/Introduction to MLA/knn source/svm_source.py
+++ Machine_Learning/Tech_With_Tim/Introduction to MLA/knn source/svm_source.py
@@ -1,3 +1,4 @@
 # Support Vector Machines (SVM)
 import sklearn
-from sklearn import linear_model, preprocessing, datasets, svm
+from sklearn import linear_model, preprocessing, datasets, svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
--- Machine_Learning/Tech_With_Tim/Introduction to MLA/svm_tutorial.py
+++ Machine_Learning/Tech_With_Tim/Introduction to MLA/svm_tutorial.py
@@ -3,6 +3,7 @@ from svm_source import *
 # Classification data set
 cancer = datasets.load_breast_cancer()
 
+# Results for features & target names
 print(cancer.feature_names)
 print(cancer.target_names)
 
@@ -12,5 +13,13 @@ y = cancer.target
 # Training & Testing
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
-# Results
-print(x_train, y_train)
+# Results for training
+classes = ['malignant','benign']
+
+clf = svm.SVC(kernel='linear')
+# need better processing to use poly, will work on obtaining/creating a machine
+clf.fit(x_train,y_train)
+
+y_prediction = clf.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_prediction)
+print(accuracy)
