54101ec8b556853bb266959e5375c1b64fff62b4
CyborgVillager <almawijonathan@gmail.com>
2020-02-01 18:12:45 -0600

    v24 Installed Tensorflow
    Installed Tensorflow: installed Tensorflow ->
            Link : https://www.tensorflow.org/tutorials/keras/classification
            file tensorflow0.py can now show images of the clothing objects

---
+++ Machine_Learning/Tech_With_Tim/TensorFlow/tensorflow0.py
@@ -0,0 +1,13 @@
+from tensor_source import *
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+# show image of the data
+plt.imshow(train_images[7])
+plt.show()
