db1f760ea4e9b54012748c5f3cae8fb1976cd169
CyborgVillager <almawijonathan@gmail.com>
2020-02-06 01:31:15 -0600

    v30 Ai_Development_1 Created
    Ai_Development_1 Created: Created a new folder to hold future information regarding A.I development. Updated
                                    tensorflow1.py to print out a list from keras.datasets.imdb.
                                    Each of these int points to certain words, helps the user to find specific information
                                    of their choosing by making the movie review into a list, now need to work on a mapping
                                    to make it easier for humans to read it

--- Machine_Learning/Tech_With_Tim/TensorFlow/tensorflow1.py
+++ Machine_Learning/Tech_With_Tim/TensorFlow/tensorflow1.py
@@ -3,3 +3,8 @@ from tensor_source import *
 
 data = keras.datasets.imdb
 (train_data,train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+
+# This results aquire the integer encoded words of 'movie reviews' from keras.datasets.imdb
+# Each of these int points to certain words, helps the user to find specific information of their choosing by
+# making the movie review into a list, now need to work on a mapping to make it easier for humans to read it
+print(train_data[0])
